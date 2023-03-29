dbdddbe194c88e106990baa3c99fc681e118886a
dtimofeev <td090833@gmail.com>
2023-03-29 18:25:18 +0300

    Added well-known Future

---
+++ From Promise to Future.js
@@ -0,0 +1,24 @@
+'use strict';
+
+class Future {
+  constructor(executor) {
+    this.executor = executor;
+  }
+  static of(value) {
+    return new Future((resolve) => resolve(value));
+  }
+  chain(fn) {
+    return new Future((resolve, reject) => this.fork(
+      (value) => fn(value).fork(resolve, reject),
+      (error) => reject(error),
+    ));
+
+  }
+  map(fn) {
+    return this.chain((value) => Future.of(fn(value)));
+
+  }
+  fork(successful, failed) {
+    this.executor(successful, failed);
+  }
+}
